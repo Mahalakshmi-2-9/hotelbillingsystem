13cd1efb32794e7d56ffe75d3143801865e9db1c
Mahalakshmi-2-9 <144028932+Mahalakshmi-2-9@users.noreply.github.com>
2025-02-24 17:49:39 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        FoodOrdering foodOrdering = new FoodOrdering();
-        foodOrdering.placeOrder();
-
-        BillGenerator billGenerator = new BillGenerator(foodOrdering);
-        billGenerator.generateBill();
-    }
-}
